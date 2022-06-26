e2d8ce7a7cb7873ca8f9ac43d3f4fbd9dd28f3c0
0SCAR0421 <oscar0421@kakao.com>
2022-06-26 17:01:53 +0900

    Add: swagger 설정파일 추가

---
+++ swaggerDoc.js
@@ -0,0 +1,20 @@
+const swaggerUi = require("swagger-ui-express")
+const swaggereJsdoc = require("swagger-jsdoc")
+
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "saessac api server",
+      description:
+        "saessac api server",
+    },
+    host : "34.168.215.145", // base-url
+    basePath : "/" // base path
+  },
+  apis: ["./server.js"], //Swagger 파일 연동
+}
+const specs = swaggereJsdoc(options)
+
+module.exports = { swaggerUi, specs }
